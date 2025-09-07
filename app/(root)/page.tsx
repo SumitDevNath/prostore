1c113989db5c9d8f3d7ef75bf4d72936c69a79cb
SumitDevNath <sumit.devnath1@gmail.com>
2025-09-08 03:29:47 +0600

    Product cart components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,10 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const HomePage = () => {
-  return <h1>ProStore</h1>;
+  return (
+    <ProductList data={sampleData.products} title="Newest Arrival" limit={4} />
+  );
 };
 
 export default HomePage;
