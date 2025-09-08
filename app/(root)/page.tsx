f73bf65d852c049943582b5a19f7f6b4e9d333ad
SumitDevNath <sumit.devnath1@gmail.com>
2025-09-09 02:10:30 +0600

    Load Products From Database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
 import ProductList from "@/components/shared/product/product-list";
-import sampleData from "@/db/sample-data";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const HomePage = () => {
-  return (
-    <ProductList data={sampleData.products} title="Newest Arrival" limit={4} />
-  );
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+  return <ProductList data={latestProducts} title="Newest Arrival" limit={4} />;
 };
 
 export default HomePage;
