54f95b320a51bb0c9a92321fdcdf6e04b5f4c06b
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-09-08 13:32:32 +0600

    PRoduct Pricing

--- components/shared/product/product-cart.tsx
+++ components/shared/product/product-cart.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
@@ -16,14 +17,17 @@ const ProductCard = ({ product }: { product: any }) => {
         />
       </CardHeader>
       <CardContent className="p-4 grid gap-4">
-        <div className="text-xs">{product.brand}</div>
+        <div className="text-xs italic font-bold">{product.brand}</div>
         <Link href={`/product/${product.slug}`}>
           <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
         <div className="flex-between gap-4">
           <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className="font-bold">{product.price}</p>
+            <ProductPrice
+              value={Number(product.price)}
+              className="text-gray-600"
+            />
           ) : (
             <p className="text-destructive">Product not available</p>
           )}
