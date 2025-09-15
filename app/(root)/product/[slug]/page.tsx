250bca3041dd84bacbe418db546bdf5e02bd266a
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-09-15 19:03:13 +0600

    Product Details

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -1,3 +1,7 @@
+import ProductPrice from "@/components/shared/product/product-price";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 import { getProductsbySlug } from "@/lib/actions/product.actions";
 import { notFound } from "next/navigation";
 
@@ -10,7 +14,67 @@ const ProductDetailsPage = async (props: {
     notFound();
   }
 
-  return <>{product.name}</>;
+  return (
+    <>
+      <section>
+        <div className="grid grid-col-1 md:grid-cols-5">
+          <div className="col-span-2"></div>
+          <div className="col-span-2 p-5">
+            <div className="flex flex-col gap-6">
+              <p>
+                {product.brand} {product.category}
+              </p>
+              <h1 className="h3-bold">{product.name}</h1>
+              <p className="italic font-extrabold font-stretch-condensed">
+                ${product.rating} of ${product.numReviews} Reviews
+              </p>
+              <div className="flex flex-col sm:flex-row sm:items-center-safe gap-3">
+                <ProductPrice
+                  value={Number(product.price)}
+                  className="w-24 rounded-full bg-green-100 text-green-700 px-5 py-2"
+                />
+              </div>
+            </div>
+            <div className="mt-10">
+              <p className="font-semibold">Description</p>
+              {product.description}
+            </div>
+          </div>
+
+          <div>
+            <Card>
+              <CardContent className="p-4">
+                <div className="mb-2 flex justify-between">
+                  <div>Price</div>
+                  <div>
+                    <ProductPrice value={Number(product.price)} />
+                  </div>
+                </div>
+                <div className="mb-2 flex justify-between">
+                  <div>Status</div>
+                  {product.stock > 0 ? (
+                    <Badge variant="outline">
+                      <span className="flex items-center space-x-2">
+                        <span className="h-2 w-2 bg-green-500 rounded-full" />
+                        <span>In Stock</span>
+                      </span>
+                    </Badge>
+                  ) : (
+                    <Badge variant="destructive">Out of Stock</Badge>
+                  )}
+                </div>
+                {product.stock > 0 && (
+                  <div className="flex justify-center items-center">
+                    <Button className="w-full">Add to Cart</Button>
+                  </div>
+                )}
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProductDetailsPage;
