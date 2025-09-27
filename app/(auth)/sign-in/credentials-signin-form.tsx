2e903b7d8509c39c3df417e8355d5497d9b8e132
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-09-27 15:29:41 +0600

    Hook Up Sign In Form

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -3,12 +3,28 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 
 const CredentialsSignInForm = () => {
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button className="w-full" disabled={pending} variant="default">
+        {pending ? "Signing In...." : "Sign In"}
+      </Button>
+    );
+  };
+
   return (
-    <form>
+    <form action={action}>
       <div className="space-y-6">
         <div>
           <Label htmlFor="email" className="my-2">
@@ -37,10 +53,13 @@ const CredentialsSignInForm = () => {
           />
         </div>
         <div>
-          <Button className="w-full" variant="default">
-            Sign In
-          </Button>
+          <SignInButton />
         </div>
+
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+
         <div className="text-sm text-center text-muted-foreground">
           Don't have an account?{" "}
           <Link
