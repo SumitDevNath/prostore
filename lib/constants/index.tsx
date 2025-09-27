8215beabc04622c657990d23eca78cf67f2b466d
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-09-27 18:28:52 +0600

    redirected url but not working

--- lib/constants/index.tsx
+++ lib/constants/index.tsx
@@ -7,5 +7,5 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 export const signInDefaultValues = {
   email: "",
-  password: "Not 123456",
+  password: "",
 };
