48182348e4d09466d6b58748ccfe23306860ead2
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-10-08 17:57:08 +0600

    Cart Zod Schema & Prisma Model

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,15 @@
-import { inserProductSchema } from "@/lib/validators";
 import z from "zod";
+import {
+  cartItemSchema,
+  inserProductSchema,
+  insertCartSchema,
+} from "@/lib/validators";
 
 export type Product = z.infer<typeof inserProductSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
