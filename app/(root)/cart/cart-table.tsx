db9a6779807e8247744afa48b6d8d1a73be70019
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-10-13 17:28:47 +0600

    insert table

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -1,13 +1,24 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
 import { Cart } from "@/types";
 import { useTransition } from "react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
 import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { formatCurrency } from "@/lib/utils";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
   const router = useRouter();
@@ -22,7 +33,119 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3"></div>
+          <div className="overflow-x-auto md:col-span-3">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Unit Price</TableHead>
+                  <TableHead className="text-right">Total</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => {
+                  const unit = Number(item.price);
+                  const lineTotal = item.qty * unit;
+                  return (
+                    <TableRow key={item.slug}>
+                      <TableCell>
+                        <Link
+                          href={`/product/${item.slug}`}
+                          className="flex items-center"
+                        >
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          />
+                          <span className="px-2">{item.name}</span>
+                        </Link>
+                      </TableCell>
+                      <TableCell className="flex-center gap-2">
+                        <Button
+                          disabled={isPending}
+                          variant="outline"
+                          type="button"
+                          onClick={() =>
+                            startTransition(async () => {
+                              const res = await removeItemFromCart(
+                                item.productId
+                              );
+                              if (!res.success) {
+                                toast.error(res.message, { closeButton: true });
+                                return;
+                              }
+                              toast.success("Removed from cart");
+                              router.refresh();
+                            })
+                          }
+                        >
+                          {isPending ? (
+                            <Loader className="w-4 h-4 animate-spin" />
+                          ) : (
+                            <Minus className="w-4 h-4" />
+                          )}
+                        </Button>
+                        <span>{item.qty}</span>
+                        <Button
+                          disabled={isPending}
+                          variant="outline"
+                          type="button"
+                          onClick={() =>
+                            startTransition(async () => {
+                              const res = await addItemToCart(item);
+                              if (!res.success) {
+                                toast.error(res.message, { closeButton: true });
+                                return;
+                              }
+                              toast.success("Added to cart");
+                              router.refresh();
+                            })
+                          }
+                        >
+                          {isPending ? (
+                            <Loader className="w-4 h-4 animate-spin" />
+                          ) : (
+                            <Plus className="w-4 h-4" />
+                          )}
+                        </Button>
+                      </TableCell>
+                      <TableCell className="text-right">
+                        {formatCurrency(item.price)}
+                      </TableCell>
+                      <TableCell className="text-right">
+                        {formatCurrency(lineTotal)}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </div>
+          <Card>
+            <CardContent className="p-4   gap-4">
+              <div className="pb-3 text-xl">
+                Subtotal ({cart.items.reduce((a, c) => a + c.qty, 0)}):
+                {formatCurrency(cart.itemsPrice)}
+              </div>
+              <Button
+                onClick={() =>
+                  startTransition(() => router.push("/shipping-address"))
+                }
+                className="w-full"
+                disabled={isPending}
+              >
+                {isPending ? (
+                  <Loader className="animate-spin w-4 h-4" />
+                ) : (
+                  <ArrowRight className="w-4 h-4" />
+                )}
+                Proceed to Checkout
+              </Button>
+            </CardContent>
+          </Card>
         </div>
       )}
     </>
