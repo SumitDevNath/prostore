3094818be1a912ab1dd420e5f29807295c566a7d
Sumit Dev Nath <sumit.devnath1@gmail.com>
2025-10-15 19:46:07 +0600

    Payment Method Action & Zod Schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import {
+  paymentMethodSchema,
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
@@ -11,6 +12,7 @@ import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "../utils";
 import { ShippingAddress } from "@/types";
+import z, { success } from "zod";
 
 // sign in the user with credentials
 export async function signInWithCredentials(
@@ -123,6 +125,32 @@ export async function updateUserAddress(data: ShippingAddress) {
     return {
       success: true,
       message: "User updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update user's payment method
+export async function userPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+    if (!currentUser) {
+      throw new Error("User not found");
+    }
+    const paymentMethod = paymentMethodSchema.parse(data);
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+    return {
+      success: true,
+      message: "User Updated Successfully",
     };
   } catch (error) {
     return { success: false, message: formatError(error) };
